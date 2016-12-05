a9bff7615874b67a04b46b38ad7c56162583c3b5
David Culbreth <androxxtraxxon>
2016-12-05 20:16:22 +0000

    IT WORKS!

--- commWithArduino.py
+++ commWithArduino.py
@@ -1,97 +1,124 @@
 ﻿import serial
 import Pathfinding
 import sys
+import time
 
-options = {}
-left = right = 0
+
+
 
 def init():
-    "This will be called at the beginning of the program to initialize everything."
-    left_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400L3-if00'
-    right_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400T8-if00'
-    left = serial.Serial(left_ard, 9600)
-    right = serial.Serial(right_ard, 9600)
-    left.write(b"x")
-    right.write(b"x")
-
-
-        # map the inputs to the function blocks
-    options = {"w" : move_forward,
-           "a" : move_left,
-           "s" : stop,
-           "d" : move_right,
-           "x" : end,
-           "u" : us_sensor,
-           "t" : servo_top,
-           "b" : servo_bottom
-           }
-    print options
-    return
-
-
+	"This will be called at the beginning of the program to initialize everything."##LINE 10
+	left_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400L3-if00'
+	right_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400T8-if00'
+	left = serial.Serial(left_ard, 9600)
+	right = serial.Serial(right_ard, 9600)
+	left.write(b"x")
+	right.write(b"x")
+	return
 
 def end():
-    "Stops the motors and disconnects the serial comm channels."
-    stop()
-    right.close()
-    left.close()
-    return
+	"Stops the motors and disconnects the serial comm channels."##LINE 20
+	stop()
+	##right.close()
+	##left.close()
+	return
 
 def stop():
-    right.write(b"x")
-    left.write(b"x")
-    return
-
+	right.write(b"x")
+	left.write(b"x")
+	return
+##LINE 30
 def move_left():
-    left.write(b"i")
-    right.write(b"o")
-    return
+	left.write(b"i")
+	right.write(b"o")
+	return
 
 def move_right():
-    right.write(b"i")
-    left.write(b"o")
-    return
+	right.write(b"i")
+	left.write(b"o")
+	return
+##LINE 40
 
-def move_forward():
-    right.write(b"f")
-    left.write(b"f")
-    return
+def move_foward():
+	right.write(b"w")
+	left.write(b"w")
+	return
 
 def move_reverse():
-    left.write(b"r")
-    right.write(b"r")
-    return
-
+	left.write(b"r")
+	right.write(b"r")
+	return
+##LINE 50
 def us_sensor():
-    left.write(b"u")
-    right.write(b"u")
-    return
-
-def loop():
-    x = raw_input("Enter Command: ")
-    print x
-    options[str(x)]
-    return
+	left.write(b"u")
+	right.write(b"u")
+	##print("Left: " + left.read()  )
+	##print("Right: " + right.read()  )
+	return
 
 def servo_top():
-    left.write(b"t")
-    right.write(b"t")
-    return
-
+	left.write(b"t")
+	right.write(b"t")
+	return
+##LINE 60
 def servo_bottom():
-    left.write(b"b")
-    right.write(b"b")
-    return
+	left.write(b"b")
+	right.write(b"b")
+	return
+
+def command(x):
+
+	if x == 'w':
+		##right.write(b"w")
+		##left.write(b"w")
+		move_foward()
+	if x == 'a':
+		move_left()
+	if x == 's':
+		move_reverse()
+	if x == 'd':
+		move_right()
+	if x == 'x':
+		stop()
+	if x == 'u':
+		us_sensor()
+	if x == 't':
+		servo_top()
+	if x == 'b':
+		servo_bottom()
+
+	return
 
 
-	
-init()
+def loop():
+	x = input("Enter Command: ")
+	print(x)
+	#options[str(x)]()
+	return
+
+
+"This will be called at the beginning of the program to initialize everything."
+left_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400L3-if00'
+right_ard='/dev/serial/by-id/usb-Intel_ARDUINO_101_AE6642SQ60400T8-if00'
+left = serial.Serial(left_ard, 9600)
+right = serial.Serial(right_ard, 9600)
+left.write(b"x")
+right.write(b"x")
 
 print("Welcome to The Raspberry Pi Controller HQ")
 
 while(True):
+	x = input("Enter Command: ")
+	print(x)
+	if x == '1':
+		end()
+		break
+	else:
+		command(x)
 
-    loop()
+
+
+
 
 end()
 
