9c04eeb7d37614b88e4585c5500f44b2173a3fa3
AnonyNotur <eduardodav1611@gmail.com>
2016-12-20 16:49:01 -0600

    Start Button
    Added the interrupts in python for the Raspberry Pi to detect the start
    button

--- commWithArduino.py
+++ commWithArduino.py
@@ -2,14 +2,20 @@
 import Pathfinding
 import sys
 import time
+import RPi.GPIO as GPIO
+GPIo.setmode(GPIO.BCM)
 
+#Setting up the Interrupt
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-"""This will be called at the beginning of the program to initialize everything."""
+#Setting up communication between arduino and raspberry pi
 
 left_ard='/dev/serial/by-id/usb-Arduino__www.arduino.cc__0043_6493833393235151C131-if00'
 right_ard='/dev/serial/by-id/usb-Arduino_LLC__www.arduino.cc__Genuino_Uno_85531303631351112162-if00'
 left = serial.Serial(left_ard, 9600)
 right = serial.Serial(right_ard, 9600)
+
+#inital commands to make sure the arduino is stop and communication is open
 
 left.write(b"9")
 right.write(b"9")
@@ -17,6 +23,7 @@ right.write(b"9")
 left.write(b"x")
 right.write(b"x")
 
+#functions that control the arduino
 
 def end():
 	"Stops the motors and disconnects the serial comm channels."##LINE 20
@@ -29,7 +36,7 @@ def stop():
 	right.write(b"x")
 	left.write(b"x")
 	return
-##LINE 30
+
 def move_right():
 	left.write(b"i")
 	right.write(b"o")
@@ -50,7 +57,7 @@ def move_reverse():
 	left.write(b"r")
 	right.write(b"r")
 	return
-##LINE 50
+
 def us_sensor():
 	left.write(b"u")
 	right.write(b"u")
@@ -62,7 +69,7 @@ def servo_top():
 	left.write(b"t")
 	right.write(b"t")
 	return
-##LINE 60
+
 def servo_bottom():
 	left.write(b"b")
 	right.write(b"b")
@@ -72,7 +79,18 @@ def restart_comm():
 	right.write(b"9")
 	return
 
+#Function that occurs when stop and/or start button are pressed
 
+def start_button_pressed():
+	#This is where the program to solve the "maze" would go
+	#for now it just makes the robot move foward
+	move_forward()
+	return
+
+#Assigned the interrupt their functions
+GPIO.add_event_detect(26, GPIO.RISING, callback = start_button_pressed, bouncetime = 300)
+
+#Switch Case that selects the commands
 def command(x):
 
 	if x == 'w':
@@ -97,6 +115,7 @@ def command(x):
 
 	return
 
+#Here is the loop that recieves input from the user
 
 print("Welcome to The Raspberry Pi Controller HQ")
 
@@ -108,6 +127,8 @@ while(True):
 		break
 	else:
 		command(x)
+
+GPIO.cleanup()
 
 
 
