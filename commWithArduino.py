3c75ee72b192b33330f73244bcbe31971fe050e3
David Culbreth <androxxtraxxon>
2017-02-21 01:34:45 +0000

    Fixed bug for motors encoders

--- commWithArduino.py
+++ commWithArduino.py
@@ -47,13 +47,14 @@ def command(x):
 		calibration_distance(int(bytes[1]))#1 for Y and 2 for X
 	"""
 	if bytes[0] == 'L':
-		encoder_calibration(int(bytes[1]), int(bytes[2]))#1 for Y and 2 for X
+		encoder_calibration(int(bytes[1]), int(bytes[2]))#Byte one(1 for Y and 2 for X), Byte Two(quantity of test)
 	if bytes[0] == 'M':
 		encoder_update()
 	if bytes[0] == 'K':
 		encoder_reset()
 	if bytes[0] == 'B':
 		encoder_current_value()
+	if bytes[0] == 'V':
 		encoder_constant_value()
 	#regarding side, 1 is for positive direction, while 2 is for negative directions
 	if bytes[0] == 'X':
